5e8a6e788b321690e9bcf62764217a88c4540233
assimon <ashang@utf8.hk>
2020-04-08 14:27:04 +0800

    修复卡密显示问题

--- app/Admin/Controllers/CardsController.php
+++ app/Admin/Controllers/CardsController.php
@@ -54,6 +54,9 @@ class CardsController extends AdminController
 
 
         });
+        $grid->actions(function ($actions) {
+            $actions->disableView();
+        });
         return $grid;
     }
 
