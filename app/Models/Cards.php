84a63480de223e2ef9d1e0c3686705311ea5e909
assimon <ashang@utf8.hk>
2020-04-09 14:00:40 +0800

    优化发布包

--- app/Models/Cards.php
+++ app/Models/Cards.php
@@ -13,45 +13,6 @@ class Cards extends Model
         return $this->belongsTo(Products::class, "product_id");
     }
 
-    //批量更新
-    public function updateBatch($multipleData = [])
-    {
-        try {
-            if (empty($multipleData)) {
-                throw new \Exception("数据不能为空");
-            }
-            $tableName = DB::getTablePrefix() . $this->getTable(); // 表名
-            $firstRow  = current($multipleData);
-
-            $updateColumn = array_keys($firstRow);
-            // 默认以id为条件更新，如果没有ID则以第一个字段为条件
-            $referenceColumn = isset($firstRow['id']) ? 'id' : current($updateColumn);
-            unset($updateColumn[0]);
-            // 拼接sql语句
-            $updateSql = "UPDATE " . $tableName . " SET ";
-            $sets      = [];
-            $bindings  = [];
-            foreach ($updateColumn as $uColumn) {
-                $setSql = "`" . $uColumn . "` = CASE ";
-                foreach ($multipleData as $data) {
-                    $setSql .= "WHEN `" . $referenceColumn . "` = ? THEN ? ";
-                    $bindings[] = $data[$referenceColumn];
-                    $bindings[] = $data[$uColumn];
-                }
-                $setSql .= "ELSE `" . $uColumn . "` END ";
-                $sets[] = $setSql;
-            }
-            $updateSql .= implode(', ', $sets);
-            $whereIn   = collect($multipleData)->pluck($referenceColumn)->values()->all();
-            $bindings  = array_merge($bindings, $whereIn);
-            $whereIn   = rtrim(str_repeat('?,', count($whereIn)), ',');
-            $updateSql = rtrim($updateSql, ", ") . " WHERE `" . $referenceColumn . "` IN (" . $whereIn . ")";
-            // 传入预处理sql语句和对应绑定数据
-            return DB::update($updateSql, $bindings);
-        } catch (\Exception $e) {
-            return false;
-        }
-    }
 
     public static function boot()
     {
