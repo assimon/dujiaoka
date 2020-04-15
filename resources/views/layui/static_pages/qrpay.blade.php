02d133acb1c2b3cb2391c5a27f76bd68ed3949ac
assimon <ashang@utf8.hk>
2020-04-15 11:34:20 +0800

    更新当面付跳转方式

--- resources/views/layui/static_pages/qrpay.blade.php
+++ resources/views/layui/static_pages/qrpay.blade.php
@@ -16,7 +16,6 @@
                     <img  src="data:image/png;base64,{!! base64_encode(QrCode::format('png')->size(200)->generate($qr_code)) !!}">
                     </div>
                     @if(Agent::isMobile() && $jump_payuri)
-                        <iframe src="{{ $jump_payuri }}" style="display:none;"></iframe>
                         <p class="errpanl" style="text-align: center"><a href="{{ $jump_payuri }}" class="layui-btn layui-btn-warm layui-btn-sm">点我打开app支付</a></p>
                     @endif
                     <p class="errpanl" style="text-align: center"><a href="{{ url('searchOrderById', ['order_id' => $orderid]) }}" class="layui-btn layui-btn-sm">我已支付</a></p>
