b3c2a52407113583c6a43b050df0188dcffa7ca6
assimon <ashang@utf8.hk>
2020-04-15 14:33:37 +0800

    修复编辑无效错误

---
+++ resources/lang/en/admin_cropper.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'choose'                => 'Select Cropper',
+    'title'                 => 'Image Cropper',
+    'done'                  => 'Crop',
+    'origin'                => 'Origin',
+    'clear'                 => 'Clear',
+];
