5da5bfe7b3f040168f407376f545e5cb3af3e707
iLay1678 <807190135@qq.com>
2020-04-27 20:30:59 +0800

    易支付增加同步回调

--- app/Http/Controllers/Pay/YipayController.php
+++ app/Http/Controllers/Pay/YipayController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Redis;
 class YipayController extends PayController
 {
     // 这里自己配置请求网关
-    const PAY_URI = '这里填写易支付的支付请求地址';
+    const PAY_URI = '易支付网关，以/结尾，不用填写php文件名';
 
     public function gateway($payway, $oid)
     {
@@ -26,7 +26,8 @@ class YipayController extends PayController
             'name'   => '在线支付-' . $this->orderInfo['order_id'],
             'money'  => (float)$this->orderInfo['actual_price'],
             'sign' =>$this->payInfo['merchant_pem'],
-            'sign_type' =>'MD5'
+            'sign_type' =>'MD5',
+            'sitename' =>config('webset.title')
         ];
         ksort($parameter); //重新排序$data数组
         reset($parameter); //内部指针指向数组中的第一个元素
@@ -44,7 +45,7 @@ class YipayController extends PayController
         $sign = md5($sign . $this->payInfo['merchant_pem']);//密码追加进入开始MD5签名
         $parameter['sign'] = $sign;
         //待请求参数数组
-        $sHtml = "<form id='alipaysubmit' name='alipaysubmit' action='".self::PAY_URI."' method='get'>";
+        $sHtml = "<form id='alipaysubmit' name='alipaysubmit' action='".self::PAY_URI."submit.php' method='get'>";
 
         foreach($parameter as $key => $val) {
             $sHtml.= "<input type='hidden' name='".$key."' value='".$val."'/>";
@@ -93,7 +94,7 @@ class YipayController extends PayController
            return redirect(site_url().'searchOrderById?order_id='.$oid);
         }else{
         	$payInfo = Pays::where('id', $cacheord['pay_way'])->first()->toArray();
-    		$data=json_decode(file_get_contents("https://pay.ifking.cn/api.php?act=order&pid=".$payInfo['merchant_id']."&key=".$payInfo['merchant_pem']."&out_trade_no=".$oid),true);
+    		$data=json_decode(file_get_contents(self::PAY_URI."api.php?act=order&pid=".$payInfo['merchant_id']."&key=".$payInfo['merchant_pem']."&out_trade_no=".$oid),true);
     	try{
     	if($data['status']=1&&$data['trade_no']){
     		$this->successOrder($oid, $data['trade_no'], $data['money']);
