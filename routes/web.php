ec29054821f46d90e59101707843934790062206
iLay1678 <807190135@qq.com>
2020-05-22 18:43:37 +0800

    格式化代码

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::post('searchOrderByAccount', 'OrdersController@searchOrderByAccount');
 Route::get('searchOrderByBrowser', 'OrdersController@searchOrderByBrowser');
 
 //api相关
-Route::group(['prefix'=>'api'],function(){
+Route::group(['prefix' => 'api'], function () {
     Route::get('typelist', 'ApiController@typelist');
     Route::post('productlist', 'ApiController@productlist');
     Route::post('proudctinfo', 'ApiController@proudctinfo');
@@ -33,35 +33,35 @@ Route::group(['prefix'=>'api'],function(){
     Route::post('postOrder', 'ApiController@postOrder');
 });
 // 支付相关
-Route::group(['prefix'=>'pay','namespace' => 'Pay'],function(){
+Route::group(['prefix' => 'pay', 'namespace' => 'Pay'], function () {
     // 支付宝
-    Route::get('alipay/{payway}/{oid}','AlipayController@gateway');
-    Route::post('alipay/notify_url','AlipayController@notifyUrl');
+    Route::get('alipay/{payway}/{oid}', 'AlipayController@gateway');
+    Route::post('alipay/notify_url', 'AlipayController@notifyUrl');
     // 微信
-    Route::get('wepay/{payway}/{oid}','WepayController@gateway');
-    Route::post('wepay/notify_url','WepayController@notifyUrl');
+    Route::get('wepay/{payway}/{oid}', 'WepayController@gateway');
+    Route::post('wepay/notify_url', 'WepayController@notifyUrl');
     // 码支付
-    Route::get('mapay/{payway}/{oid}','MapayController@gateway');
-    Route::post('mapay/notify_url','MapayController@notifyUrl');
+    Route::get('mapay/{payway}/{oid}', 'MapayController@gateway');
+    Route::post('mapay/notify_url', 'MapayController@notifyUrl');
     // Paysapi
-    Route::get('paysapi/{payway}/{oid}','PaysapiController@gateway');
-    Route::post('paysapi/notify_url','PaysapiController@notifyUrl');
+    Route::get('paysapi/{payway}/{oid}', 'PaysapiController@gateway');
+    Route::post('paysapi/notify_url', 'PaysapiController@notifyUrl');
     // payjs
-    Route::get('payjs/{payway}/{oid}','PayjsController@gateway');
-    Route::post('payjs/notify_url','PayjsController@notifyUrl');
+    Route::get('payjs/{payway}/{oid}', 'PayjsController@gateway');
+    Route::post('payjs/notify_url', 'PayjsController@notifyUrl');
     // 易支付
-    Route::get('yipay/{payway}/{oid}','YipayController@gateway');
-    Route::get('yipay/notify_url','YipayController@notifyUrl');
-    Route::get('yipay/return_url','YipayController@returnUrl');
+    Route::get('yipay/{payway}/{oid}', 'YipayController@gateway');
+    Route::get('yipay/notify_url', 'YipayController@notifyUrl');
+    Route::get('yipay/return_url', 'YipayController@returnUrl');
     // paypal
-    Route::get('paypal/{payway}/{oid}','PaypalPayController@gateway');
-    Route::get('paypal/return_url','PaypalPayController@returnUrl');
-    Route::post('paypal/notify_url','PaypalPayController@notifyUrl');
+    Route::get('paypal/{payway}/{oid}', 'PaypalPayController@gateway');
+    Route::get('paypal/return_url', 'PaypalPayController@returnUrl');
+    Route::post('paypal/notify_url', 'PaypalPayController@notifyUrl');
     // Mugglepay
-    Route::get('mugglepay/{payway}/{oid}','MugglepayController@gateway');
-    Route::post('mugglepay/notify_url','MugglepayController@notifyUrl');
+    Route::get('mugglepay/{payway}/{oid}', 'MugglepayController@gateway');
+    Route::post('mugglepay/notify_url', 'MugglepayController@notifyUrl');
     // V免签
-    Route::get('vpay/{payway}/{oid}','VpayController@gateway');
-    Route::get('vpay/notify_url','VpayController@notifyUrl');
-    Route::get('vpay/return_url','VpayController@returnUrl');
+    Route::get('vpay/{payway}/{oid}', 'VpayController@gateway');
+    Route::get('vpay/notify_url', 'VpayController@notifyUrl');
+    Route::get('vpay/return_url', 'VpayController@returnUrl');
 });
