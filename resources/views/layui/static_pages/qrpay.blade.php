97e36632dbf27a0284f6e39fc3d95fea368069d0
iLay1678 <807190135@qq.com>
2020-05-24 11:51:38 +0800

    更换电脑端隐藏唤起app支付按钮的方法，以此修复可能存在的苹果端兼容问题

--- resources/views/layui/static_pages/qrpay.blade.php
+++ resources/views/layui/static_pages/qrpay.blade.php
@@ -12,14 +12,12 @@
                         <p style="color: #1E9FFF;font-size: 20px;font-weight: 500; text-align: center" >支付方式：[{{ $payname }}], 请打开APP扫码支付！有效期{{ config('app.order_expire_date') }}分钟</p>
                     </div>
                     <div style="text-align: center; width: 100%; border: #1E9FFF solid 1px;">
-                    <p class="product-pay-price">支付金额: {{ $actual_price }}</p>
-                    <img  src="data:image/png;base64,{!! base64_encode(QrCode::format('png')->size(200)->generate($qr_code)) !!}">
+                        <p class="product-pay-price">支付金额: {{ $actual_price }}</p>
+                        <img  src="data:image/png;base64,{!! base64_encode(QrCode::format('png')->size(200)->generate($qr_code)) !!}">
                     </div>
-                    @if(Agent::isMobile() && strstr($jump_payuri, 'qr.alipay.com'))
-                       <p class="errpanl" style="text-align: center"><a href='' id='toalipay' class="layui-btn layui-btn-warm layui-btn-sm">打开支付宝支付</a></p>
-                  <script>var schemeurl = 'alipays://platformapi/startapp?appId=20000067&url='+encodeURIComponent('{{ $jump_payuri }}');
-                document.getElementById("toalipay").href=schemeurl;</script>
-                    @endif
+                    <div class="layui-hide-md">
+                        <p class="errpanl" style="text-align: center"><a href="{{ $jump_payuri }}" class="layui-btn layui-btn-warm layui-btn-sm">点我打开app支付</a></p>
+                    </div>
                 </div>
 
 
@@ -46,7 +44,6 @@
                         }, function(){
                             window.location.href = '/'
                         });
-
                     }
                     if (res.code == 200) {
                         window.clearTimeout(timer);
@@ -57,11 +54,8 @@
                         });
                     }
                 }
-
             };
             var timer = window.setInterval(function(){$.ajax(getting)},5000);
-
         })
-
     </script>
 @stop
