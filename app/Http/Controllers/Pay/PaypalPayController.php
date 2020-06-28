f08ca2dfa18074207dc6acf299f59e17b8ddc5ef
我若为王 <807190135@qq.com>
2020-06-28 09:43:14 +0800

    Update PaypalPayController.php

--- app/Http/Controllers/Pay/PaypalPayController.php
+++ app/Http/Controllers/Pay/PaypalPayController.php
@@ -50,7 +50,7 @@ class PaypalPayController extends PayController
         }
         $product = $this->orderInfo['product_name'];
         try {
-            $price = number_format($this->getUsdCurrency($this->orderInfo['actual_price']), 2);
+            $price = number_format($this->getUsdCurrency($this->orderInfo['actual_price']), 2 , '.' , '');
         } catch (\Exception $exception) {
             return $this->error($exception->getMessage());
         }
