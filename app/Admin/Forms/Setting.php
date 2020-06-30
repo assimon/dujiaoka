cc7cc2a424ed0ba79740bfc89595bbfe953dae05
assimon <ashang@utf8.hk>
2020-06-30 18:52:33 +0800

    删除多余代码

--- app/Admin/Forms/Setting.php
+++ app/Admin/Forms/Setting.php
@@ -42,12 +42,6 @@ class Setting extends Form
         $this->email('manage_email', __('Sys manage email'))->rules('required')->help("用于接收待处理订单提示");
         $this->text('langs', __('Sys Langs'))->help('默认为中文zh-CN,不会翻译语言包不要改');
         $this->UEditor('notice', __('Sys notice'));
-        $this->text('email_driver', __('Mail driver'))->help('一般为smtp');
-        $this->text('mail_host', __('Mail host'));
-        $this->text('mail_port', __('Mail port'));
-        $this->text('mail_username', __('Mail username'));
-        $this->text('mail_password', __('Mail password'));
-        $this->text('mail_encryption', __('Mail encryption'))->help('ssl或者tls');
         $this->textarea('footer', __('Sys footer'))->help('可以填写一些统计代码或者icp备案信息');
         //$this->radio('instock', __('Sys in stock monitor'))->options([1 => '开启', 2 => '关闭'])->default(1);
 
