d1c1592abe55c24da74e436ae9ec62218a88b1b4
assimon <ashang@utf8.hk>
2020-07-03 12:28:50 +0800

    修复一处语言包

--- app/Admin/Forms/Setting.php
+++ app/Admin/Forms/Setting.php
@@ -41,7 +41,7 @@ class Setting extends Form
         $this->textarea('description', __('Sys description'))->rules('required');
         $this->email('manage_email', __('Sys manage email'))->rules('required')->help("用于接收待处理订单提示");
         $this->text('langs', __('Sys Langs'))->help('默认为中文zh-CN,不会翻译语言包不要改');
-        $this->radio('verify_code', __('Sys in stock monitor'))->options([1 => '开启', 2 => '关闭'])->default(1);
+        $this->radio('verify_code', __('Verify Code'))->options([1 => '开启', 2 => '关闭'])->default(1);
         $this->UEditor('notice', __('Sys notice'));
         $this->textarea('footer', __('Sys footer'))->help('可以填写一些统计代码或者icp备案信息');
 
