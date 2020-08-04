5347afe8ac5f68c2ffa31cb0487b0d4fd44e0012
assimon <ashang@utf8.hk>
2020-08-04 11:17:18 +0800

    极验开关

--- config/app.php
+++ config/app.php
@@ -238,5 +238,5 @@ return [
     'shtemplate' => env('SH_TEMPLATE', 'layui'),
     'paypal_sandebox' => env('PAYPAL_SANDBOX', true),
     'order_expire_date' => env('ORDER_EXPIRE_DATE', 3),
-
+    'shgeetest' => env('SH_GEETEST', false),
 ];
