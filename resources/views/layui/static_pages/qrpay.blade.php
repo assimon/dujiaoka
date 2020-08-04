08a3b7310d8022144fa56a7d7a105b4e8ca9a1b5
assimon <ashang@utf8.hk>
2020-08-04 10:16:45 +0800

    修复扫码支付成功后用户点击x不跳转卡密界面问题

--- resources/views/layui/static_pages/qrpay.blade.php
+++ resources/views/layui/static_pages/qrpay.blade.php
@@ -49,7 +49,8 @@
                     if (res.code == 200) {
                         window.clearTimeout(timer);
                         layer.alert("{{ __('prompt.payment_successful') }}", {
-                            icon: 1
+                            icon: 1,
+                            closeBtn:0
                         }, function(){
                             window.location.href = "{{ url('searchOrderById', ['order_id' => $orderid]) }}"
                         });
