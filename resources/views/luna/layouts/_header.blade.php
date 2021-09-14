6fa4535174e9e0dae509cf8b920043f8689beb92
Ashang <Ashang@utf8.hk>
2021-09-14 20:20:19 +0800

    fix:解决luna模板依赖问题

--- resources/views/luna/layouts/_header.blade.php
+++ resources/views/luna/layouts/_header.blade.php
@@ -4,7 +4,7 @@
     <title>{{ isset($page_title) ? $page_title : '' }} | {{ dujiaoka_config_get('title') }}</title>
     <meta name="Keywords" content="{{ dujiaoka_config_get('keywords') }}">
     <meta name="Description" content="{{ dujiaoka_config_get('description')  }}">
-    <link rel="stylesheet" href="/assets/layui/css/layui.css">
+    <link rel="stylesheet" href="/assets/luna/layui/css/layui.css">
     <link rel="stylesheet" href="/assets/luna/main.css">
     <link rel="shortcut icon" href="/assets/style/favicon.ico" />
     @if(\request()->getScheme() == "https")
--- resources/views/luna/layouts/_script.blade.php
+++ resources/views/luna/layouts/_script.blade.php
@@ -6,7 +6,7 @@
         mobile_order : '{{ __('luna.mobile_order') }}'
     };
 </script>
-<script src="/assets/layui/layui.js"></script>
+<script src="/assets/luna/layui/layui.js"></script>
 <script src="/assets/luna/js/jquery-3.4.1.min.js"></script>
 <script src="/assets/luna/main.js"></script>
-<script src="/assets/layui/lay/modules/layer.js"></script>
+<script src="/assets/luna/layui/modules/layer.js"></script>
