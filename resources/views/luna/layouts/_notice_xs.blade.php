69677b2b04d92cf7faf7477aaef4360e4d79d8e5
风轻云断 <156348933@qq.com>
2022-05-23 11:25:12 +0800

    luna模板增加移动端公告，增加边框优化公告显示效果

---
+++ resources/views/luna/layouts/_notice_xs.blade.php
@@ -0,0 +1,14 @@
+<div class="main layui-hide-xs">
+  <div class="layui-row">
+    <div class="layui-col-sm12">
+        <div class="main-box">
+        <div class="title">
+              <p class="tit">{{ __('dujiaoka.site_announcement') }}:</p>
+          </div>
+          <div class="goods">
+              <div class="tips">{!! dujiaoka_config_get('notice') !!}</div>
+          </div>
+      </div>
+    </div>
+  </div>
+</div>
