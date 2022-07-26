7e1a497b12ffedd21dde5d9695c295af61d839ba
风轻云断 <156348933@qq.com>
2022-07-26 23:11:23 +0800

    1. 后台增加邮件测试，方便检测邮箱配置问题 2. 卡密支持循环

---
+++ resources/lang/zh_CN/email-test.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The file was created by Assimon.
+ *
+ * @author    ZhangYiQiu<me@zhangyiqiu.net>
+ * @copyright ZhangYiQiu<me@zhangyiqiu.net>
+ * @link      http://zhangyiqiu.net/
+ */
+
+return [
+    'labels' => [
+        'success' => '发送成功',
+        'to' => '收件人',
+        'title' => '邮件标题',
+        'body' => '邮件内容',
+    ],
+
+    'fields' => [
+        
+    ],
+    'options' => [
+    ],
+    'rule_messages' => [
+    ]
+];
