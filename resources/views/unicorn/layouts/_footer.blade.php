15cb67fcbc15e5d3743106b00126084c4df57434
outtime <i@timewk.cn>
2023-03-18 12:50:34 +0800

    替换版权链接为仓库链接

--- resources/views/unicorn/layouts/_footer.blade.php
+++ resources/views/unicorn/layouts/_footer.blade.php
@@ -3,7 +3,7 @@
     <div class="row">
         <div class="col-12 text-center">
             <div class="author">
-                Powered by：<a href="http://dujiaoka.com">@独角数卡</a>
+                Powered by：<a href="https://github.com/assimon/dujiaoka">@独角数卡</a>
             </div>
             <div class="custom">
                 {!! dujiaoka_config_get('footer') !!}
