85fb78ecb3cdb9d4dc90347e1573b32145646deb
snail <snail.lzq@gmail.com>
2024-01-16 09:53:47 +0800

    fix docker link 访问redis 时报参数类型错误

--- config/database.php
+++ config/database.php
@@ -130,7 +130,7 @@ return [
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', '6379'),
+            'port' => (int) env('REDIS_PORT', '6379'),
             'database' => env('REDIS_DB', '0'),
         ],
 
@@ -138,7 +138,7 @@ return [
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', '6379'),
+            'port' => (int) env('REDIS_PORT', '6379'),
             'database' => env('REDIS_CACHE_DB', '1'),
         ],
 
